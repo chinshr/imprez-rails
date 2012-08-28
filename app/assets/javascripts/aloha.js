41a4567860fad48a351f8a46f77d828de9c2c2c0
Juergen Fesslmeier <jfesslmeier@gmail.com>
2012-08-28 10:37:19 -0700

    Removing files

--- app/assets/javascripts/aloha.js
+++
@@ -1,6 +0,0 @@
-//= xrequire aloha-editor/aloha-config
-//= xrequire aloha-editor/lib/aloha-jquery-noconflict
-//= require jquery
-//= require jquery_ujs
-//= require aloha-editor/lib/require
-//= require aloha-editor/lib/aloha-full.js
