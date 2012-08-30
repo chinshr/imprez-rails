ff7767ca1f81ef56d2c8b6be9230f77da6c8af99
Juergen Fesslmeier <jfesslmeier@gmail.com>
2012-08-29 19:35:12 -0700

    Adding search highlighting

--- app/assets/javascripts/application-postpage.js
+++ app/assets/javascripts/application-postpage.js
@@ -13,6 +13,20 @@
 //= require impress
 //= require twitter/bootstrap
 
+
+/* jQuery selector to match exact text inside an element
+*  :containsExact()     - case insensitive
+*  :containsExactCase() - case sensitive
+*  :containsRegex()     - set by user ( use: $(el).find(':containsRegex(/(red|blue|yellow)/gi)') )
+*/
+$.extend($.expr[':'],{
+  containsRegexp: function(a,i,m){
+    var regreg =  /^\/((?:\\\/|[^\/])+)\/([mig]{0,3})$/,
+    reg = regreg.exec(m[3]);
+    return reg ? RegExp(reg[1], reg[2]).test($.trim(a.innerHTML)) : false;
+  }
+});
+
 $(function() {
   impress().init();
   
@@ -62,8 +76,66 @@ $(function() {
     event.preventDefault();
   });
   
-});
+  $("#nav-search").submit(function() {
+    var text = $("#nav-search input").val();
+    var found = $('.step *:containsRegexp("/' + text + '/gi")');
+    impress().goto($(found.first()).closest(".step").attr("id"));
+    event.preventDefault();
+  });
+  
+  // BEGIN:search
+  var that = this;
+  that.input = $("#nav-search input");
 
+  // handles searching the document
+  that.performSearch = function() {
+
+    // create a search string
+    var phrase = that.input.val().replace(/^\s+|\s+$/g, "");          
+    phrase = phrase.replace(/\s+/g, "|");
+
+    // remove all highlighters
+    $(".highlight").each(function(i, v) {$(v).replaceWith($(v).text())});
+
+    // make sure there are a couple letters
+    if(phrase.length < 1) {
+      return;
+    }
+
+    // append the rest of the expression
+    phrase = ["\\b(", phrase, ")"].join("");
+
+    // search for any matches
+    var count = 0;
+    $(".step h1, .step h2, .step p, .step q").each(function(i, v) {
+      var block = $(v);
+      block.html(
+        block.html().replace(
+          new RegExp(phrase, "gi"), 
+          function(match) {
+            count++;
+            return ["<span class='highlight'>", match, "</span>"].join("");
+          })
+        );
+      });
+
+      // update the count
+      // $(".result-count").text(count + " results on this page!");
+
+      // clear this search attempt
+      // should be gone anyways...
+      that.search = null;
+    };
+
+    that.search;
+    that.input.keyup(function(event) {
+      if (that.search) { clearTimeout(that.search); }
+      that.search = setTimeout(that.performSearch, 300);
+    });
+
+  });
+  // END:search
+  
 Aloha.ready(function() {
   $ = Aloha.jQuery;
   $('.step').aloha();
